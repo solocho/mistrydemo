d4b192e08fa3114034e6f3a2b85e522c5e9fbf75
solocho <solocho264@gmail.com>
2025-03-22 23:02:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 function toggleMenu() {
-    let menu = document.querySelector(".nav-links");
-    menu.classList.toggle("active");
-}
-function toggleMenu() {
-    let menu = document.querySelector(".left-h");
-    menu.classList.toggle("active");
-}
-function toggleMenu() {
     const menu = document.querySelector(".mobile-menu");
     menu.classList.toggle("active");
 }
-
